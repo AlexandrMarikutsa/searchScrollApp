5a9f8167f53584e7dfefa0694600072c72860a0a
AlexandrMarikutsa <a.marikutsa@ukr.net>
2015-10-20 11:52:46 +0300

    changed classes

---
+++ app2/src/main/java/adapter/MyAdapter.java
@@ -0,0 +1,65 @@
+package adapter;
+
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.SectionIndexer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import constants.ArrayOfStrings;
+import service.Alphavit;
+
+public class MyAdapter extends ArrayAdapter<String> implements SectionIndexer {
+
+    private String[] strings = ArrayOfStrings.STRINGS;
+    private List<String> items ;
+    private HashMap<String, Integer> alphaIndexer;
+
+    public MyAdapter(Context context, int textViewResourceId, List<String> objects) {
+        super(context, textViewResourceId, objects);
+        arrayToList();
+        int size = items.size();
+        alphaIndexer = new HashMap<>();
+
+        for (int x = 0; x < size; x++) {
+            String s = items.get(x);
+            String ch = s.substring(0, 1);
+            ch = ch.toUpperCase();
+            if (!alphaIndexer.containsKey(ch)) {
+                alphaIndexer.put(ch, x);
+            }
+        }
+        Set<String> sectionLetters = alphaIndexer.keySet();
+        ArrayList<String> sectionList = new ArrayList<String>(sectionLetters);
+        Collections.sort(sectionList);
+        String[]sections = new String[sectionList.size()];
+        sectionList.toArray(sections);
+    }
+
+    public Object[] getSections() {
+        return Alphavit.getAlphaLetters(items);
+    }
+
+    public int getPositionForSection(int i) {
+
+        Log.d("-------", "getPositionForSection " + i);
+        return (int) (getCount() * ((float)i/(float)getSections().length));
+    }
+
+    public int getSectionForPosition(int i) {
+        return 0;
+    }
+
+    private void arrayToList(){
+        items = new ArrayList<>();
+        for (String ch : strings) {
+            items.add(ch);
+        }
+    }
+}
--- app2/src/main/java/com/demo/develop/app2/Main.java
+++ app2/src/main/java/com/demo/develop/app2/Main.java
@@ -12,12 +12,12 @@ import android.widget.SectionIndexer;
 import java.util.ArrayList;
 import java.util.List;
 
+import adapter.MyAdapter;
 import constants.ArrayOfStrings;
 import service.Alphavit;
 
 public class Main extends Activity {
-
-    List<String> items;
+    private List<String> items;
     public static final String TAG = Main.class.getCanonicalName();
 
     @Override
@@ -33,31 +33,9 @@ public class Main extends Activity {
                 items.add(ch);
         }
 
-        listView.setAdapter(new IndexingArrayAdapter(this, android.R.layout.simple_list_item_1, items));
+        listView.setAdapter(new MyAdapter(this, android.R.layout.simple_list_item_1, items));
 
         SideSelector sideSelector = (SideSelector) findViewById(R.id.side_selector);
         sideSelector.setListView(listView);
-
     }
-
-    public class IndexingArrayAdapter extends ArrayAdapter<String> implements SectionIndexer {
-        public IndexingArrayAdapter(Context context, int textViewResourceId, List<String> objects) {
-            super(context, textViewResourceId, objects);
-        }
-
-        public Object[] getSections() {
-            return Alphavit.getAlphaLetters(items);
-        }
-
-        public int getPositionForSection(int i) {
-
-            Log.d(TAG, "getPositionForSection " + i);
-            return (int) (getCount() * ((float)i/(float)getSections().length));
-        }
-
-        public int getSectionForPosition(int i) {
-            return 0;
-        }
-    }
-
 }
