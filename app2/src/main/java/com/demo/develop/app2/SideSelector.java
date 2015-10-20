30586d1513dcc6a41a08b8336c5babb817e62232
AlexandrMarikutsa <a.marikutsa@ukr.net>
2015-10-20 16:33:30 +0300

    changed searching by char

--- app2/src/main/java/com/demo/develop/app2/SideSelector.java
+++ app2/src/main/java/com/demo/develop/app2/SideSelector.java
@@ -10,6 +10,9 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.SectionIndexer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import constants.ArrayOfStrings;
 import service.Alphavit;
 
@@ -19,7 +22,9 @@ public class SideSelector extends View {
     public static String[] alphabet;
     public static final int BOTTOM_PADDING = 10;
 
+    private List<Section> sectionsOnSelector;
     private Section section;
+    private float charHeight;
     private SectionIndexer selectionIndexer = null;
     private ListView list;
     private Paint paint;
@@ -46,8 +51,6 @@ public class SideSelector extends View {
         paint.setColor(0xFFA6A9AA);
         paint.setTextSize(20);
         paint.setTextAlign(Paint.Align.CENTER);
-//        alphabet = ArrayOfStrings.STRINGS;
-//        alphabet = Alphavit.getAlphaLetters(alphabet);
     }
 
     public void setListView(ListView _list) {
@@ -72,31 +75,40 @@ public class SideSelector extends View {
             if (selectionIndexer == null) {
                 selectionIndexer = (SectionIndexer) list.getAdapter();
             }
-            if (y >= 0 && y <= section.y) {
-                Log.e("----------", "" + section.position);
-                int position = selectionIndexer.getPositionForSection(section.position);
-                if (position == -1) {
-                    return true;
+            for (Section s: sectionsOnSelector) {
+                if (y >= s.y && y <= (s.y+charHeight)) {
+                    Log.e("-----POSITION-----", "" + s.position);
+                    int position = selectionIndexer.getPositionForSection(s.position);
+                    if (position == -1) {
+                        return true;
+                    }
+                    list.setSelection(position);
                 }
-                list.setSelection(position);
             }
         }
         return true;
     }
 
     protected void onDraw(Canvas canvas) {
-
+        sectionsOnSelector = new ArrayList<>();
         int viewHeight = getPaddedHeight();
-        float charHeight = ((float) viewHeight) / 30;
+        charHeight = ((float) viewHeight) / 30;
         float widthCenter = getMeasuredWidth() / 2;
 
-        canvas.drawText(String.valueOf(sections[5]), widthCenter, charHeight, paint);
-        section = new Section();
-        section.position =5;
-        section.y = (int) charHeight;
-        canvas.drawText(String.valueOf("*"), widthCenter, charHeight + (charHeight), paint);
-        canvas.drawText(String.valueOf("*"), widthCenter, charHeight + (2 * charHeight), paint);
-        canvas.drawText(String.valueOf(sections[sections.length - 1]), widthCenter, charHeight + (3 * charHeight), paint);
+        for(int i = 0; i < 4; i++) {
+            canvas.drawText(String.valueOf(sections[i]), widthCenter, (i+1)*charHeight + charHeight, paint);
+            section = new Section(i, (int) ((i+1)*charHeight));
+            sectionsOnSelector.add(section);
+        }
+        canvas.drawText(String.valueOf("*"), widthCenter, 5*charHeight + (charHeight), paint);
+        canvas.drawText(String.valueOf("*"), widthCenter, charHeight + (6 * charHeight), paint);
+
+        for(int i = 0; i < 4; i++) {
+            canvas.drawText(String.valueOf(sections[(sections.length - 1) - i]), widthCenter, ((sections.length - 1) - i)*charHeight +charHeight, paint);
+            section = new Section((sections.length - 1) - i, (int)(((sections.length - 1) - i)*charHeight));
+            sectionsOnSelector.add(section);
+        }
+//        canvas.drawText(String.valueOf(sections[sections.length - 1]), widthCenter, charHeight + (3 * charHeight), paint);
         super.onDraw(canvas);
     }
 
@@ -107,5 +119,10 @@ public class SideSelector extends View {
     public class Section {
         public int position;
         public int y;
+
+        public Section(int position, int y) {
+            this.position = position;
+            this.y = y;
+        }
     }
 }
