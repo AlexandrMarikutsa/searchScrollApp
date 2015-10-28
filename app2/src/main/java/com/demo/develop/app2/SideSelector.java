e4c1c05ac4cb40c51e17adf8f07198fbdb2116b5
AlexandrMarikutsa <a.marikutsa@ukr.net>
2015-10-28 17:35:18 +0200

    changed principally logic

--- app2/src/main/java/com/demo/develop/app2/SideSelector.java
+++ app2/src/main/java/com/demo/develop/app2/SideSelector.java
@@ -5,16 +5,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.os.Build;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewTreeObserver;
-import android.view.WindowManager;
-import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,6 +22,7 @@ public class SideSelector extends View {
     private static int BOTTOM = 4;
     private OnCustomEventListener listener;
     private List<Section> sectionsOnSelector;
+    private List<Section> sectionsAll;
     private Section section;
     private Section currentSection;
     private int pressedSection = 0;
@@ -47,18 +41,147 @@ public class SideSelector extends View {
 
     public SideSelector(Context context) {
         super(context);
-        setListLetters()
-        countNumberOfElements();
     }
 
     public SideSelector(Context context, AttributeSet attrs) {
         super(context, attrs);
-        countNumberOfElements();
     }
 
     public SideSelector(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        countNumberOfElements();
+    }
+
+    public void setListLetters(Set<Character> sectionsSet) {
+
+        Character[]sections = new Character[sectionsSet.size()];
+        sectionsSet.toArray(sections);
+        Arrays.sort(sections);
+        this.sections = sections;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+        int y = (int) event.getY();
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
+            for (Section s: sectionsAll) {
+                if (y >= (s.y - charHeight) && y <= s.y) {
+                    if(listener !=null){
+                        listener.getChar(s.name);
+                        pressedSection = s.position;
+                        currentSection = s;
+
+                    }
+                    invalidate();
+                }
+            }
+        }
+        return true;
+    }
+
+    protected void onDraw(Canvas canvas) {
+        initTextSize();
+        getAllSections();
+        drawSections(canvas);
+        super.onDraw(canvas);
+    }
+
+    public void initTextSize(){
+        int height  = getHeight();
+        int width = getWidth();
+        int widthPart = width/2;
+        if (widthPart > textSize){
+            if(height/widthPart >= 7){
+                textSize = height/(height/widthPart);
+            }else {
+                textSize = height/7;
+            }
+        }
+        init();
+        widthCenter = getMeasuredWidth() / 2;
+        charHeight = textSize;
+    }
+
+    private int countNumberOfElements(){
+        return getHeight()/textSize;
+    }
+
+    public class Section {
+        private Character name;
+        private int position;
+        private int y;
+        private boolean visible;
+
+        public Section(Character name, int position) {
+            this.name = name;
+            this.position = position;
+        }
+
+        public Section(Character name, int position, int y) {
+            this.name = name;
+            this.position = position;
+            this.y = y;
+        }
+    }
+
+    private void drawNotAllSections(Canvas canvas){
+        int numOfUpAndDownSec = (int) ((getHeight() - BOTTOM + charHeight) / charHeight);
+        if((numOfUpAndDownSec % 2) > 0){
+            upSections = numOfUpAndDownSec/2;
+            downSections = upSections;
+            for(int i = 0; i < upSections; i++){
+                Section section = sectionsAll.get(i);
+                section.y = (int) (i * charHeight + charHeight);
+                drawSection(canvas, section);
+            }
+            upPoint = (int) ((upSections - 1) * charHeight + charHeight + charHeight/2 - radius);
+            downPoint = (int) (upPoint + charHeight/2);
+            drawPoint(canvas, upPoint);
+            drawPoint(canvas, downPoint);
+
+            for(int i = 1; i < downSections + 1; i++){
+                Section section = sectionsAll.get(i + sectionsAll.size() - downSections - 1);
+                section.y = (int) (i * charHeight + downPoint);
+                drawSection(canvas, section);
+            }
+        }else {
+
+        }
+    }
+
+    public void drawSections(Canvas canvas){
+        /*if all letters are on the screen*/
+        if(countNumberOfElements() >= sectionsAll.size()){
+            int i = 0;
+            for(Section section: sectionsAll){
+                i++;
+                int thisCharHeight = (getHeight() - BOTTOM)/ sections.length;
+                section.y = i * thisCharHeight;
+                drawSection(canvas, section);
+            }
+        }else {
+            drawNotAllSections(canvas);
+        }
+    }
+
+    private void getAllSections(){
+        sectionsAll = new ArrayList<>();
+        for(int i = 0; i < sections.length; i++) {
+            sectionsAll.add(new Section(sections[i], i));
+        }
+    }
+
+    public void setCustomEventListener(OnCustomEventListener eventListener) {
+        listener = eventListener;
+    }
+
+    public void drawSection(Canvas canvas, Section section){
+        if (section.position != pressedSection) {
+            canvas.drawText(String.valueOf(section.name), widthCenter, section.y, paint);
+        } else {
+            canvas.drawText(String.valueOf(section.name), widthCenter, section.y, paintCurrentLetter);
+        }
     }
 
     private void init() {
@@ -78,73 +201,16 @@ public class SideSelector extends View {
         paintPoint.setColor(0xFFA6A9AA);
         paintPoint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
         paintPoint.setTextAlign(Paint.Align.CENTER);
-    }
-    public void setListLetters(Set<Character> sectionsSet) {
-
-        Character[]sections = new Character[sectionsSet.size()];
-        sectionsSet.toArray(sections);
-        Arrays.sort(sections);
-        this.sections = sections;
+        paintPoint.setTextSize(textSize/2);
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        super.onTouchEvent(event);
-        int y = (int) event.getY();
+    public void drawPoint(Canvas canvas, int point){
+        canvas.drawCircle(widthCenter, point, radius, paintPoint);
+    }
 
-        if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
-            for (Section s: sectionsOnSelector) {
-                if (y >= (s.y - charHeight) && y <= s.y) {
-                    if(listener !=null){
-                        listener.getChar(s.name);
-                        pressedSection = s.position;
-                        currentSection = s;
+    private void getSections(){
+        if(getHeight() <= charHeight*sections.length) {
 
-                    }
-                    changeSelectorView(s.position);
-                }
-            }
         }
-        return true;
-    }
-
-    private int countNumberOfElements(){
-        if (getWidth()/2 > textSize){
-            textSize = getHeight()/7;
-        }
-        return getHeight()/textSize;
-    }
-
-    protected void onDraw(Canvas canvas) {
-        init();
-        super.onDraw(canvas);
-    }
-
-    private List<Section> getSectionList(){
-        List<Section> sections = new ArrayList<>();
-        for(int i = 1; i <= countNumberOfElements(); i++){
-            Section section = new Section(sections[i], i, )
-        }
-    }
-
-    private void changeSelectorView(int position){
-        this.invalidate();
-    }
-
-    public class Section {
-        private Character name;
-        private int position;
-        private int y;
-        private boolean visible;
-
-        public Section(Character name, int position, int y) {
-            this.name = name;
-            this.position = position;
-            this.y = y;
-        }
-    }
-
-    public void setCustomEventListener(OnCustomEventListener eventListener) {
-        listener = eventListener;
     }
 }
